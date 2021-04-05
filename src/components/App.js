51dcd4ada4ed58897c9bd94f4beeee428ed85afd
Erik Huang <ekhu94@gmail.com>
2021-04-04 22:24:30 -0700

    scaffold redux, thunk, and apis file, several Components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import GamesList from './GamesList';
+
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default App;
